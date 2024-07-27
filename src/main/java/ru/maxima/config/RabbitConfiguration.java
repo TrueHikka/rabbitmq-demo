7399bd82b14cdff6a84ada87a04a663a44b6d791
Vitaly Rubis <vitalyrubis@gmail.com>
2024-07-27 16:32:58 +0300

    ref-2

--- src/main/java/ru/maxima/config/RabbitConfiguration.java
+++ src/main/java/ru/maxima/config/RabbitConfiguration.java
@@ -1,8 +1,7 @@
 package ru.maxima.config;
 
 import lombok.Data;
-import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -34,9 +33,91 @@ public class RabbitConfiguration {
 
     @Bean
     public Queue myFirstQueue() {
-        return new Queue("myFirstQueue", false);
+//        return QueueBuilder.durable("myFirstQueue").build();
+       return new Queue("myFirstQueue", false);
     }
 
+    @Bean
+    public Queue mySecondQueue() {
+//        return QueueBuilder.durable("mySecondQueue").build();
+        return new Queue("mySecondQueue", false);
+    }
+
+    @Bean
+    public Queue myThirdQueue() {
+//        return QueueBuilder.durable("mySecondQueue").build();
+        return new Queue("myThirdQueue", false);
+    }
+
+//    @Bean
+//    public Queue myThirdQueue() {
+//        return new Queue("myThirdQueue");
+//    }
+
+    //* 1 - FanoutExchange
+//    @Bean
+//    public FanoutExchange myExchange() {
+//        return new FanoutExchange("myExchange");
+//    }
+//
+//    @Bean
+//    public Binding binding1() {
+//        return BindingBuilder.bind(myFirstQueue()).to(myExchange());
+//    }
+//
+//    @Bean
+//    public Binding binding2() {
+//        return BindingBuilder.bind(mySecondQueue()).to(myExchange());
+//    }
+
+
+    //* 2 - DirectExchange
+    @Bean
+    public DirectExchange myDirectExchange() {
+//        return  ExchangeBuilder.directExchange("myDirectExchange").durable(true).build();
+
+        return new DirectExchange("myDirectExchange");
+    }
+
+    @Bean
+    public Binding binding1() {
+        return BindingBuilder.bind(myFirstQueue()).to(myDirectExchange()).with("user");
+    }
+
+    @Bean
+    public Binding binding2() {
+        return BindingBuilder.bind(mySecondQueue()).to(myDirectExchange()).with("admin");
+    }
+
+    @Bean
+    public Binding binding3() {
+        return BindingBuilder.bind(myThirdQueue()).to(myDirectExchange()).with("hr");
+    }
+
+    //* 3 - TopicExchange
+//    @Bean
+//    public TopicExchange myTopicExchange() {
+////        return ExchangeBuilder.topicExchange("myTopicExchange").durable(true).build();
+//        return new TopicExchange("myTopicExchange");
+//    }
+//    @Bean
+//    public Binding binding1() {
+//        return BindingBuilder.bind(myFirstQueue()).to(myTopicExchange()).with("first.*"); //все сообщения начинаются с "first"
+//    }
+//
+//    @Bean
+//    public Binding binding2() {
+//        return BindingBuilder.bind(mySecondQueue()).to(myTopicExchange()).with("#.second.#"); //все сообщения заканчиваются на "second"
+//    }
+//
+//    @Bean
+//    public Binding binding3() {
+//        return BindingBuilder.bind(myThirdQueue()).to(myTopicExchange()).with("*.third.*"); //
+//    }
+
+
+
+    //! Заместо этого используем RabbitListener.java
 //    @Bean
 //    public SimpleMessageListenerContainer simpleMessageListenerContainer() {
 //        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
